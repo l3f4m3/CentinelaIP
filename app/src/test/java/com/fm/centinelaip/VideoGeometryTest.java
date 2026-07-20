481815f0634fab70d552345debb87e1a999ac9ce
l3f4m3 <166322484+l3f4m3@users.noreply.github.com>
2026-07-19 19:46:48 -0500

    test(video): validar escala llenar y ajustar

--- app/src/test/java/com/fm/centinelaip/VideoGeometryTest.java
+++ app/src/test/java/com/fm/centinelaip/VideoGeometryTest.java
@@ -6,16 +6,34 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public final class VideoGeometryTest {
-    @Test public void portraitVideoInsideWideViewReducesHorizontalScale() {
-        float[] scale = VideoGeometry.fitScale(1500, 340, 478, 850);
+    @Test public void portraitVideoInWideViewFillsByGrowingVerticalScale() {
+        float[] scale = VideoGeometry.displayScale(1500, 340, 478, 850, true);
+        assertEquals(1f, scale[0], 0.0001f);
+        assertEquals((1500f / 340f) / (478f / 850f), scale[1], 0.0001f);
+    }
+
+    @Test public void portraitVideoInWideViewFitsByReducingHorizontalScale() {
+        float[] scale = VideoGeometry.displayScale(1500, 340, 478, 850, false);
         assertEquals(1f, scale[1], 0.0001f);
         assertEquals((478f / 850f) / (1500f / 340f), scale[0], 0.0001f);
     }
 
-    @Test public void wideVideoInsideTallViewReducesVerticalScale() {
-        float[] scale = VideoGeometry.fitScale(500, 1000, 1920, 1080);
-        assertEquals(1f, scale[0], 0.0001f);
-        assertEquals((500f / 1000f) / (1920f / 1080f), scale[1], 0.0001f);
+    @Test public void wideVideoInTallViewFillsByGrowingHorizontalScale() {
+        float[] scale = VideoGeometry.displayScale(500, 1000, 1920, 1080, true);
+        assertEquals((1920f / 1080f) / (500f / 1000f), scale[0], 0.0001f);
+        assertEquals(1f, scale[1], 0.0001f);
+    }
+
+    @Test public void sharedModeControlsTextureScale() {
+        DisplayModeStore.setFillScreen(true);
+        float[] fill = VideoGeometry.fitScale(1000, 500, 500, 1000);
+        assertEquals(1f, fill[0], 0.0001f);
+        assertEquals(4f, fill[1], 0.0001f);
+
+        DisplayModeStore.setFillScreen(false);
+        float[] fit = VideoGeometry.fitScale(1000, 500, 500, 1000);
+        assertEquals(0.25f, fit[0], 0.0001f);
+        assertEquals(1f, fit[1], 0.0001f);
     }
 
     @Test public void scaledSizePreservesAspectAndDoesNotUpscale() {
